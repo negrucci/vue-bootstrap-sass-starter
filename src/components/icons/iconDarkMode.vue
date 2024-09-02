2903a7d4255844f99f87533e009cbdb2b1c72d7a
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-09-02 19:32:31 -0300

    feat: add icon components for dark and light mode
    - Added iconDarkMode component to represent dark mode.
    - Added iconLightMode component to represent light mode.
    - Integrated these components into toggleColorMode.vue to switch between dark and light themes visually.

---
+++ src/components/icons/iconDarkMode.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+<svg viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke-width="0.43200000000000005"></g><g id="SVGRepo_iconCarrier"><path d="M9.272 2.406a1 1 0 0 0-1.23-1.355C6.59 1.535 5.432 2.487 4.37 3.55a11.399 11.399 0 0 0 0 16.182c4.518 4.519 11.51 4.261 15.976-.205 1.062-1.062 2.014-2.22 2.498-3.673A1 1 0 0 0 21.55 14.6c-3.59 1.322-7.675.734-10.434-2.025-2.765-2.766-3.328-6.83-1.844-10.168Z"></path></g></svg>
+</template>
+
+<style scoped lang="scss"></style>
