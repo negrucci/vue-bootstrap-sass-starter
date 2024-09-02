8759e272dde8448fde721d791e94e622c38321fd
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-09-02 12:54:34 -0300

    refactor: update language switcher to dropdown with icon
    - Replace button group with dropdown menu for language selection
    - Add icon component to dropdown button for enhanced UI
    - Update styles to handle light and dark themes with Bootstrap
    - Modify template to display language options in dropdown list with detailed labels
    - Ensure smooth transition and maintain scoped styles

--- src/components/toggleLanguage.vue
+++ src/components/toggleLanguage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useLanguageSwitcher } from '@/composables/useLanguageSwitcher.js'
+import iconWorld from './icons/iconWorld.vue'
 
 const { locale, availableLocales, changeLanguage } = useLanguageSwitcher()
 const animationState = ref(false)
@@ -13,22 +14,48 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="btn-group" role="group" aria-label="Toggle Language" style="width: 140px">
+    <div class="dropdown w-auto px-0">
         <button
-            @click="changeLanguage(lang)"
-            v-for="lang in availableLocales"
-            :key="lang"
+            class="btn border-0 p-2"
             type="button"
-            class="btn btn-outline-primary"
-            :class="{ active: lang === locale, 'no-transition': !animationState }"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            :class="{ 'no-transition': !animationState }"
         >
-            {{ lang }}
+            <iconWorld class="icon" width="42px" height="42px" />
         </button>
+        <ul class="dropdown-menu p-1">
+            <li @click="changeLanguage(lang)" v-for="lang in availableLocales" :key="lang">
+                <a class="dropdown-item d-flex px-2 py-2 rounded-1" href="#" :class="{ active: lang === locale }">
+                    <div style="width: 47px">{{ lang }}</div>
+                    <div class="px-1"></div>
+                    <div class="text-capitalize">
+                        {{ lang === 'en-US' ? 'English (United States)' : 'Português (Brasil)' }}
+                    </div>
+                </a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <style scoped lang="scss">
 .no-transition {
     transition: none !important;
+}
+[data-bs-theme='light'] {
+    .btn:hover {
+        background-color: rgba(var(--bs-dark-rgb), 0.2);
+    }
+    .icon {
+        fill: var(--bs-dark);
+    }
+}
+[data-bs-theme='dark'] {
+    .btn:hover {
+        background-color: rgba(var(--bs-light-rgb), 0.2);
+    }
+    .icon {
+        fill: var(--bs-light);
+    }
 }
 </style>
