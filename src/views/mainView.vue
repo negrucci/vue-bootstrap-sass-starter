716252dd775e9f88e1e6fb8b512a783fb40d2820
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-09-21 18:47:04 -0300

    feat: Update layout to include Bootstrap tooltips
    - Added Bootstrap Tooltip initialization in the onMounted lifecycle hook for better user experience.
    - Adjusted template structure for improved responsiveness and styling of dataComp.

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -1,17 +1,25 @@
 <script setup>
-import fontComp from '@/components/fontComp.vue'
+import { onMounted } from 'vue'
+import { Tooltip } from 'bootstrap'
 import dataComp from '@/components/dataComp.vue'
 import mainHeader from '@/components/mainHeader.vue'
+
+onMounted(() => {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
+  tooltipList
+})
 </script>
 
 <template>
     <mainHeader />
     <div class="container-fluid user-select-none">
-        <div class="row row-cols-1 justify-content-center gap-3">
-            <fontComp class="col"/>
-            <dataComp class="col"/>
+        <div class="container-xxl px-2 px-sm-3 px-md-4 px-lg-4 px-xxl-0">
+            <div class="row row-cols-1 justify-content-center gap-3">
+                <dataComp class="col px-0 border"/>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss"></style>
