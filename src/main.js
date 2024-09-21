3f8960b4c1fb46c86ae5c6d3410a6d5f2e4ff21e
FelipeNegrucci <felipe.negrucci@protonmail.com>
2024-09-21 18:21:26 -0300

    fix: update bootstrap import method
    - Commented out the direct import of bootstrap JS.
    - Imported bootstrap as a module for better compatibility.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import('@/assets/bootstrap/bootstrap.scss')
-import('../node_modules/bootstrap/dist/js/bootstrap.bundle.min')
+// import('../node_modules/bootstrap/dist/js/bootstrap.bundle.min')
+import * as bootstrap from 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
+
 
 const app = createApp(App)
 app.use(router)
